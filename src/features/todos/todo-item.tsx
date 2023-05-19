ab77adbfb38b5e3858ea8a5199570ccf65e343be
sirkaelan <kaelanfreakerman6@gmail.com>
2023-05-19 14:03:22 +0300

    Add handler to remove todo

--- src/features/todos/todo-item.tsx
+++ src/features/todos/todo-item.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { useRemoveTodo } from "./TodoContext";
 
 type TodoItemProps = {
   id: string;
   content: string;
 };
 
+type ClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>;
+
 export const TodoItem = ({ id, content }: TodoItemProps): JSX.Element => {
+  const removeTodo = useRemoveTodo();
+
+  const handleRemoveClick = (e: ClickEvent): void => removeTodo(id);
+
   return (
     <div>
       <span>{content}</span>
-      <button>Delete</button>
+      <button onClick={handleRemoveClick}>Remove</button>
     </div>
   );
 };
