835d16c2d68c16dc260f06b3930672e615353e3b
SirKaelan <kaelanfreakerman6@gmail.com>
2023-05-27 15:36:39 +0300

    Update some dependencies for testing, Create utility file for tests, Fix first test

--- src/features/todos/__test__/create-todo-form.test.tsx
+++ src/features/todos/__test__/create-todo-form.test.tsx
@@ -1,15 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/react";
+import { setup } from "../../../testUtils/setup";
 import { CreateTodoForm } from "../create-todo-form";
 
 describe("Unit tests for create-todo-form component", () => {
   test("should be able to type in input", async () => {
-    render(<CreateTodoForm placeholderText="Enter a task..." />);
+    const { user } = setup(
+      <CreateTodoForm placeholderText="Enter a task..." />
+    );
     const todoInputEl: HTMLInputElement = screen.getByRole("textbox", {
       name: /enter todo/i,
     });
-    // Fix "act" warning
-    userEvent.type(todoInputEl, "Go out for a walk");
+    await user.click(todoInputEl);
+    await user.keyboard("Go out for a walk");
     expect(todoInputEl).toHaveValue("Go out for a walk");
   });
 });
