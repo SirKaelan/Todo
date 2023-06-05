a62b58e652858f1c3b4184550c33e622785a7866
sirkaelan <kaelanfreakerman6@gmail.com>
2023-06-05 12:29:28 +0300

    Add CSS style reset and SASS variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./sass-partials/_global.scss";
 import App from "./App";
 import { UIStateProvider } from "./features/ui";
 import { PageDataProvider } from "./pages";
