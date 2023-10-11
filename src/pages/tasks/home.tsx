e08c5e851ecd8435bbc4b8e0521d06c8bcfd322b
SirKaelan <kaelanfreakerman6@gmail.com>
2023-10-11 14:13:14 +0300

    Scrap Grid system idea, but still keep it

--- src/pages/tasks/home.tsx
+++ src/pages/tasks/home.tsx
@@ -9,29 +9,19 @@ import { Grid } from "features/ui";
 
 export const Home = (): JSX.Element => {
   return (
-    // <main className={styles.tasks}>
-    //   <TaskNavigation />
-
-    //   <article className={styles.tasks__manager}>
-    //     <div className={styles.tasks__manager__container}>
-    //       <Routes>
-    //         <Route index element={<Inbox />} />
-    //         <Route path="completed" element={<Completed />} />
-    //       </Routes>
-    //     </div>
-    //   </article>
-    // </main>
-
+    // One time use for fun
     <Grid container>
-      <Grid item lg={1}>
+      <Grid item md={2} lg={1}>
         <TaskNavigation />
       </Grid>
-      <Grid item lg={10}>
+      <Grid item xs={12} sm={12} md={9} lg={9}>
         <article className={styles.tasks__manager}>
-          <Routes>
-            <Route index element={<Inbox />} />
-            <Route path="completed" element={<Completed />} />
-          </Routes>
+          <div className={styles.tasks__manager__container}>
+            <Routes>
+              <Route index element={<Inbox />} />
+              <Route path="completed" element={<Completed />} />
+            </Routes>
+          </div>
         </article>
       </Grid>
     </Grid>
